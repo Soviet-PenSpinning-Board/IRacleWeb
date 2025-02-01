b6772a4b2142dae67ca4fd8e669d038a89e119bf
IRacle <zornik2006@yandex.ru>
2025-02-01 21:12:58 +0300

    арт любит тостых женщин с эмбрионами внутри

---
+++ TestPens/wwwroot/js/battlesLocalTime.js
@@ -0,0 +1,7 @@
+﻿document.querySelectorAll(".battle-start-time").forEach(elem => {
+    const utcDate = elem.dataset.utc;
+    if (utcDate) {
+        const localDate = moment.utc(utcDate).local();
+        elem.textContent = localDate.format("DD MMM YYYY, HH:mm");
+    }
+});
--- TestPens/wwwroot/js/clickBattlesVideo.js
+++ TestPens/wwwroot/js/clickBattlesVideo.js
@@ -12,4 +12,4 @@
             iframe.style.display = '';
         }
     })
-})
+});
